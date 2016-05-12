e0bae092df5c1b5cdc1556376a577d12725c9b0c
DanialBeg <danny00b@gmail.com>
2016-05-12 13:37:12 -0700

    Update Encryption.java

--- Triple_DES-Encryption-Decryption/src/Encryption.java
+++ Triple_DES-Encryption-Decryption/src/Encryption.java
@@ -11,25 +11,24 @@ public class Encryption implements Encryption_Decryption{
 
 	public static void main(String[] args) throws Exception{
 		Encryption_Decryption a = new Encryption();
-		String b = a.encrypt("textToDecrypt", "SecretKey");
+		String b = a.encrypt("jarjar", "jarjarissnoke");
 		System.out.println(b);
 	}
 
 	public String encrypt(String text, String key) throws Exception{
-		MessageDigest md = MessageDigest.getInstance("SHA-1");
-		byte[] digestOfPassword = md.digest(key.getBytes("utf-8"));
-		byte[] keyBytes = Arrays.copyOf(digestOfPassword, 24);
+		MessageDigest msg = MessageDigest.getInstance("SHA-1");
+		byte[] psw = msg.digest(key.getBytes("utf-8"));
+		byte[] bytes = Arrays.copyOf(psw, 24);
 		
-		SecretKey keys = new SecretKeySpec(keyBytes, "DESede");
+		SecretKey keys = new SecretKeySpec(bytes, "DESede");
 		Cipher cipher = Cipher.getInstance("DESede");
 		cipher.init(Cipher.ENCRYPT_MODE, keys);
 		
 		byte[] plainTextBytes = text.getBytes("utf-8");
-	    byte[] buf = cipher.doFinal(plainTextBytes);
-	    byte [] base64Bytes = Base64.encodeBase64(buf);
-	    String base64EncryptedString = new String(base64Bytes);
-	    
-	    return base64EncryptedString;
+	        byte[] buf = cipher.doFinal(plainTextBytes);
+	        byte [] base64Bytes = Base64.encodeBase64(buf);
+	        String string = new String(base64Bytes);
+	        return string;
 	}
 
 	public String decrypt(String text, String key) throws Exception{
